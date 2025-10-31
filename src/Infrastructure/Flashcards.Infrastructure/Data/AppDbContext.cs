e97f3eeeb9ea497e762b5fad964d2975c049a209
Rydner Obeal <C1054-23@itmlyceumalabang.onmicrosoft.com>
2025-10-31 13:30:32 +0800

    Commit 1: DbContext

--- src/Infrastructure/Flashcards.Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Flashcards.Infrastructure/Data/AppDbContext.cs
@@ -1,14 +1,17 @@
 using Flashcards.Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Flashcards.Infrastructure.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Deck> Decks => Set<Deck>();
     public DbSet<Flashcard> Flashcards => Set<Flashcard>();
+    public DbSet<StudyResult> StudyResults => Set<StudyResult>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -16,6 +19,12 @@ public class AppDbContext : DbContext
             .HasMany(d => d.Flashcards)
             .WithOne(f => f.Deck!)
             .HasForeignKey(f => f.DeckId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<StudyResult>()
+            .HasOne(sr => sr.Deck!)
+            .WithMany()
+            .HasForeignKey(sr => sr.DeckId)
             .OnDelete(DeleteBehavior.Cascade);
 
         base.OnModelCreating(modelBuilder);
